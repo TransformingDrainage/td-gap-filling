4333355d91f3828e7278f792affe2142525d4981
IASTATE\gio <giorgichi@gmail.com>
2019-08-19 08:28:41 -0500

    Generate missing data for DPAC
    Create 1000 amputed datasets with different proportions of missing data (5% to 45%) using MAR (Missing At Rrandom) mechanism.

--- 02_data_preparation.R
+++ 02_data_preparation.R
@@ -2,7 +2,7 @@
 # Versions differ from each other by artificially created missing values (so we can check predictions)
 
 source(file = '00_project_settings.R')
-
+library(mice)
 
 
 # Read Data ---------------------------------------------------------------
@@ -28,4 +28,71 @@ original_input <- df %>%
 original_input %T>%
   write_rds(path = 'Data/Input_Data/RDS/original_input.rds') %>%
   write_csv(path = 'Data/Input_Data/original_input.csv')
+
+
+
+# Amputation --------------------------------------------------------------
+
+# Do the amputation for each site individually, and format as below
+
+# DPAC ....................................................................
+original_input %>% 
+  filter(siteid == "DPAC") %>%
+  filter(!is.na(flow)) %>%
+  select(-comments, -siteid) %>%
+  spread(plotid, flow) -> DPAC
+
+# set parameters for amputation process
+
+# percentage of missing data
+my_prop <- seq(0.05, 0.45, 0.10)
+
+# set 3 patterns of missing data: 
+# (a) SW is missing, but not NE,
+# (b) NE is missing, but SW is not,
+# (c) both NE and SW are missing
+my_pattern <- tibble(year = 1, date = 1, rain = 1, NE = c(1,0,0), SW = c(0,1,0)) 
+
+# it's more frequent plots failing the same time (pattern C), than each one separately
+# this can be done by assigning frequency to each pattern
+my_freq <- c(0.1, 0.1, 0.8)
+
+# this can be of use when implementing MNAR
+my_weights <- tibble(year = 1, date = 1, rain = 1, NE = c(1,0,0), SW = c(0,1,0)) 
+
+# this matrix helps to distribute missing values equally throught the period (no TAILS, etc)
+my_odds <- matrix(1, nrow = 3, ncol = 4)
+
+DPAC_amp <- vector('list', length = length(my_prop) * 200)
+temp_amp <- vector('list', length = 11)
+# Generating missing data at DPAC
+for (i in seq_along(my_prop)) {
+  for (j in 1:200) {
+    list_num <- 200*(i-1) + j
+    # apply percent to remove to each year separately
+    for (k in 2006:2016) {
+      DPAC %>% 
+        filter(year == k) %>%
+        ampute(patterns = my_pattern,
+               freq = my_freq,
+               prop = my_prop[i], 
+               mech = 'MAR', 
+               cont = FALSE,
+               odds = my_odds) -> results
+      temp_amp[[k-2005]]  <- results$amp
+    }
+    DPAC_amp[[list_num]] <- bind_rows(temp_amp) %>% 
+      mutate(date = as.Date(date), prop = my_prop[i])
+  }
+}
+
+# check the last output
+DPAC_amp[[1000]]
+
+# save amputated DPAC data
+write_rds(DPAC_amp, path = 'Data/Input_Data/RDS/DPAC_amp_MAR.rds')
+DPAC_amp %>% bind_rows() %>% write_csv('Data/Input_Data/DPAC_amp_MAR.csv')
+
+
+
 

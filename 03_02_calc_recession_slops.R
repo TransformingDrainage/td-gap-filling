12d8222a2a030faaf133e7b8d4815a3cf992c784
IASTATE\gio <giorgichi@gmail.com>
2020-08-31 14:29:10 -0500

    Calculate slope for each scenario separately
    - Before averge slopes were calculated using all data and then uniformly utilized in each scenario. As a result, I would predict missing values for 2-year scenarios using slopes calculated based on 11 years of data.
    
    Resolves: issue #8

--- 03_02_calc_recession_slops.R
+++ 03_02_calc_recession_slops.R
@@ -5,14 +5,6 @@ source(file = '00_project_settings.R')
 
 
 
-# Read Data ---------------------------------------------------------------
-
-df <- read_rds('Data/Input_Data/RDS/DPAC_amp_MAR_pred_module1.rds') 
-
-
-
-# Calculate recession slopes ----------------------------------------------
-
 # Create functions to select peak and inflection points
 peak_inflection <- function(df) {
   df %>%
@@ -56,45 +48,60 @@ peak_inflection <- function(df) {
            slope = change/days)
 }
 
-# Calculate recession slopes
-recession_slopes <-
-  df %>%
-  unnest() %>%
-  gather(flow_type, flow, starts_with('flow')) %>%
-  arrange(simulation, prop, plotid, flow_type, date) %>%
-  group_by(simulation, prop, plotid, flow_type) %>%
-  nest() %>%
-  # log-transform flow data 
-  mutate(
-    data = map(data,
-               ~ .x %>%
-                 # ADD 2 EXTRA READINGS TO AVOID PROBLEMS IN PEAK/INFLECTION SELECTION
-                 add_row(flow = c(0, 0), .before = TRUE) %>%
-                 # make sure to handle log(0)
-                 mutate(ln_flow = ifelse(flow == 0, NA, log(flow)))
-    )
-  ) %>%
-  # find PEAK and INFLECTION points
-  mutate(data = map(data, peak_inflection))
 
 
-# save recession slopes so YOU DO NOT HAVE TO CALCULATE THEM AGAIN
-recession_slopes %>%
-  arrange(plotid, as.integer(simulation)) %>%
-  write_rds('Data/Input_Data/RDS/DPAC_amp_MAR_recession_slopes.rds')
+# Calculate Recession Slopes ----------------------------------------------
+# Read input files
+dpac_files <- list.files('Data/Inter_Data/Phase2_Imputation/DPAC/', full.names = TRUE)
+
+for (i in dpac_files) {
+  DNAME <- str_extract(i, pattern = 'DPAC_Y.{4}')
+  read_rds(i) %>%
+    unnest() %>%
+    gather(flow_type, flow, starts_with('flow')) %>%
+    arrange(simulation, prop, plotid, flow_type, date) %>%
+    group_by(simulation, prop, plotid, flow_type) %>%
+    nest() %>%
+    # log-transform flow data 
+    mutate(
+      data = map(data,
+                 ~ .x %>%
+                   # ADD 2 EXTRA READINGS TO AVOID PROBLEMS IN PEAK/INFLECTION SELECTION
+                   add_row(flow = c(0, 0), .before = TRUE) %>%
+                   # make sure to handle log(0)
+                   mutate(ln_flow = ifelse(flow == 0, NA, log(flow)))
+      )
+    ) %>%
+    # find PEAK and INFLECTION points
+    mutate(data = map(data, peak_inflection)) %>%
+    arrange(plotid, as.integer(simulation)) %>%
+    # save recession slopes so YOU DO NOT HAVE TO CALCULATE THEM AGAIN
+    write_rds(paste0('Data/Inter_Data/Phase3_Imputation/DPAC/recession_slopes/', 
+                     DNAME, '_recession_slopes.rds'),
+              compress = 'xz')
+}
 
 
-# Calculate ave recession slope
-recession_slopes %>%
-  unnest(data) %>%
+
+# Calculate Ave Recession Slope -------------------------------------------
+recession_slopes <- vector('list')
+for (i in list.files('Data/Inter_Data/Phase3_Imputation/DPAC/recession_slopes', full.names = TRUE)) {
+  recession_slopes[[i]] <- read_rds(i)
+}
+
+recession_slopes %>% 
+  bind_rows(.id = 'id') %>%
+  mutate(id = str_extract(id, 'Y._.{2}')) %>%
   # select "peak" POINTs, because they correspond to recession (falling) limb 
-  filter(POINT == 'peak') %>%
-  filter(!is.infinite(slope)) %>%
+  mutate(data = map(data, ~ .x %>% filter(POINT == 'peak',
+                                          !is.infinite(slope)) %>%
+                      ungroup())) %>%
+  unnest(data) %>%
   # save intermediate data for plotting in Global Environment
   {. ->> recession_slopes_to_plot} %>%
   # calculate average number of days between peak and first point of inflection
   # and the slope of recession limb 
-  group_by(simulation, prop, flow_type, plotid, season) %>%
+  group_by(id, simulation, prop, flow_type, plotid, season) %>%
   summarise(ave_days = mean(days),
             # calculate trimmed, geometric and harmonic means since the distributions of slopes are right-skewed 
             ave_slope = mean(slope),
@@ -103,12 +110,15 @@ recession_slopes %>%
             ave_slope_harm = 1/(mean(1/slope))         # most conservative
   ) %>%
   ungroup() %>%
-  write_csv('Data/Input_Data/DPAC_amp_MAR_ave_recession_slopes.csv')
+  write_rds('Data/Inter_Data/Phase3_Imputation/DPAC/DPAC_ave_recession_slopes.rds', 
+            compress = 'xz')
 
+# save slopes
+write_rds(recession_slopes_to_plot, 'Data/Inter_Data/Phase3_Imputation/DPAC/DPAC_all_recession_slopes.rds', 
+          compress = 'xz')
 
 
 # Plot recession slopes
-
 recession_slopes_to_plot %>%
   filter(flow_type == 'flow_pred') %>%
   ggplot(aes(flow, slope, col = days)) +
@@ -132,7 +142,7 @@ ggsave('Figs/models/recession_slopes_distribution.png',
 
 
 recession_slopes_to_plot %>%
-  group_by(simulation, prop, flow_type, plotid, season) %>%
+  group_by(id, simulation, prop, flow_type, plotid, season) %>%
   summarise(ave_days = mean(days),
             ave_slope = mean(slope),
             ave_slope_trim = mean(slope, trim = 0.1),  # less conservative

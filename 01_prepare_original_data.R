98b889418e05c804c1c1378cb5877cc15bbb7470
Giorgi Chighladze <giorgichi@gmail.com>
2020-10-30 18:11:55 -0500

    Add if snows

--- 01_prepare_original_data.R
+++ 01_prepare_original_data.R
@@ -13,9 +13,32 @@ drive_ls(as_id('1zblZuTiEUdZOq1_IHgO_gEtR018TidRq'), pattern = 'tile_flow') %>%
 
 
 # Read Data ---------------------------------------------------------------
-flow <- read_csv('Data/Input_Data/Original/tile_flow.csv', guess_max = 80000)
+flow <- 
+  read_csv('Data/Input_Data/Original/tile_flow.csv', guess_max = 80000)
 
-weather <- read_csv('Data/Input_Data/Original/daily_tile_flow.csv')
+weather <- 
+  # instead of using the final weather file, we used old data file because it
+  # already has precipitation from relative weather stations each year at SERF_IA
+  # NOTE: at SERF_IA there were 3 sources for precipitation with different degree
+  # of reliability for individual year, according to the site personals feedback
+  read_csv('Data/Input_Data/Original/daily_tile_flow.csv')
+
+snowfall <- 
+  readxl::read_excel('Data/Input_Data/Original/DPAC_snowfall_2007-2018.xlsx',
+                     skip = 7)
+
+
+
+# Combine Snowfall and Weather Data ---------------------------------------
+weather_snow <-
+  snowfall %>% 
+  mutate(siteid = 'DPAC',
+         date = as.Date(Date),
+         snowing = ifelse(as.numeric(SNOW) == 0 | is.na(as.numeric(SNOW)), 
+                          0, 1)) %>%
+  select(siteid, date, snowing) %>%
+  right_join(weather, by = c("siteid", "date"))
+  
 
 
 
@@ -25,19 +48,15 @@ data <-
   filter(siteid %in% c('IA_Washington', 'IN_Randolph'),
          dwm_treatment == 'Free Drainage') %>%
   select(id = siteid, plotid, date, tile_flow, tile_flow_filled)  %>%
-  # instead of using the final weather file, we used old data file because it
-  # already has precipitation from relative weather stations each year at SERF_IA
-  # NOTE: at SERF_IA there were 3 sources for precipitation with different degree
-  # of reliability for individual year, according to the site personals feedback
-  full_join(weather, by = c('plotid', 'date')) %>%
-  select(id, siteid, plotid, year, date, tile_flow, tile_flow_filled, precip_on_site) 
+  full_join(weather_snow, by = c('plotid', 'date')) %>%
+  select(id, siteid, plotid, year, date, tile_flow, tile_flow_filled, precip_on_site, snowing) 
   
 
 
 # Select Data to Be Analyzed ----------------------------------------------
 data_to_analyze <-
   data %>%
-  select(siteid, plotid, year, date, flow = tile_flow_filled, rain = precip_on_site) %>%
+  select(siteid, plotid, year, date, flow = tile_flow_filled, rain = precip_on_site, snowing) %>%
   # rename SERF_IA to SERF for ease to type only
   mutate(siteid = ifelse(siteid == 'SERF_IA', 'SERF', siteid)) %>%
   # include only years of interest for each site
